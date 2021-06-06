1eeb1d912e9d9b8c8ea37039cd9649590ccbe43d
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-06 02:23:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,6 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -18,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array
-  array.shift
+  array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
