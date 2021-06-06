d3b6902374ced89d984be2ec46c8df08b0363b90
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-06 02:21:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,62 +2,37 @@ def create_an_empty_array
   []
 end
 
-puts create_an_empty_array
-
 def create_an_array
-  create_an_array = ["tom", "sarah", "josh", "mike"]
+  ["One", 2, true, :four]
 end
 
-puts create_an_array
-
-def add_element_to_end_of_array
-  add_element_to_end_of_array = create_an_array << "steve"
+def add_element_to_end_of_array(array, element)
 end
 
-puts add_element_to_end_of_array
-
-def add_element_to_start_of_array
-  add_element_to_start_of_array = create_an_array.unshift("laura")
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
 end
 
-puts add_element_to_start_of_array
-
-def remove_element_from_end_of_array 
-  remove_element_from_end_of_array = create_an_array.pop
+def remove_element_from_end_of_array(array)
+  array.pop
 end
 
-puts remove_element_from_end_of_array 
-
-def remove_element_from_start_of_array 
-  remove_element_from_start_of_array = create_an_array.shift
+def remove_element_from_start_of_array
+  array.shift
 end
-
-puts remove_element_from_start_of_array 
 
 def retrieve_element_from_index(array, index_number)
-   retrieve_element_from_index(array, index_number) = ["tom", "sarah", "josh", "mike"]
-   retrieve_element_from_index(array, index_number)[1] 
+  array[index_number]
 end
-
-puts retrieve_element_from_index(array, index_number)
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array(array) = ["tom", "sarah", "josh", "mike"]
-  retrieve_first_element_from_array(array)[0]
+  array.first
 end
 
-puts retrieve_first_element_from_array(array)
-
-def retrieve_last_element_from_array(array) 
-  retrieve_last_element_from_array(array) = ["tom", "sarah", "josh", "mike"]
-  retrieve_last_element_from_array(array)[-1]
+def retrieve_last_element_from_array(array)
+  array.last
 end
-
-puts retrieve_last_element_from_array(array) 
 
 def update_element_from_index(array, index_number, element)
-  update_element_from_index(array, index_number, element) = ["tom", "sarah", "josh", "mike"]
-  pdate_element_from_index(array, index_number, element)[2] = "charlie"
+  array[index_number] = element
 end
-
-puts update_element_from_index(array, index_number, element)
